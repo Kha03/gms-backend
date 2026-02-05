b6ee0d94f26ccc2b90d58c485347461b916852da
Kha <minhkha20031996@gmail.com>
2026-02-05 15:10:30 +0700

    fix crors

--- src/main/java/com/thang/gms_backend/config/OpenApiConfig.java
+++ src/main/java/com/thang/gms_backend/config/OpenApiConfig.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -21,4 +24,10 @@ import org.springframework.context.annotation.Configuration;
         bearerFormat = "JWT"
 )
 public class OpenApiConfig {
+        @Bean
+        public OpenAPI customOpenAPI() {
+                return new OpenAPI()
+                        .addServersItem(new Server().url("https://gms-backend-production.up.railway.app").description("Production Server"))
+                        .info(new io.swagger.v3.oas.models.info.Info().title("GMS API").version("1.0"));
+        }
 }
